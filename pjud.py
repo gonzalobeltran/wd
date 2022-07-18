a19570c02fb12476358d47281c6004ba3fdf003e
Gonzalo Beltrán <gonzalobeltran@gmail.com>
2022-07-18 00:39:58 -0400

    Funcional

--- pjud.py
+++ pjud.py
@@ -1,7 +1,6 @@
 
 import time, datetime
 import pandas as pd
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,16 +8,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
 def login(usuario, pwd):
-    driver.get("https://oficinajudicialvirtual.pjud.cl/indexN.php#")
-
+    driver.get("https://oficinajudicialvirtual.pjud.cl/home/index.php")
+    
     driver.find_element(By.CLASS_NAME, "dropbtn").click()
-    driver.find_element(By.ID, "btnSegClave").click()
-    time.sleep(1)
-
-    driver.find_element(By.ID, "rut").send_keys(usuario)
-    driver.find_element(By.ID, "password").send_keys(pwd)
-    driver.find_element(By.ID, "btnSegundaClaveIngresar").click()
-    time.sleep(2)
+    driver.find_element(By.XPATH, "//div[@id='myDropdown']/a").click()
+    driver.find_element(By.ID, "uname").send_keys(usuario)
+    driver.find_element(By.ID, "pword").send_keys(pwd)
+    driver.find_element(By.ID, "login-submit").click()
+    time.sleep(4)
 
     driver.find_element(By.CLASS_NAME, "fa-university").click()
     time.sleep(1)
@@ -27,12 +24,16 @@ def login(usuario, pwd):
     time.sleep(1)
 
     driver.find_elements(By.CLASS_NAME, "switch")[6].click()
-    time.sleep(1)
+    time.sleep(2)
 
     # Selecciona todos los tipos de causas
     estadoCausas = Select(driver.find_element(By.ID, 'estadoCausaMisCauFam'))
     for i in range(12):
         estadoCausas.select_by_index(str(i))
+
+def logout():
+    driver.find_element(By.XPATH, "//a[@onclick='salir();']").click()
+    time.sleep(1)
 
 def nom_tribunal(t):
     res = t
@@ -48,7 +49,7 @@ def nom_tribunal(t):
         res = 'CENTRO DE MEDIDAS CAUTELARES DE SANTIAGO'
     return(res)
 
-def busca_causa(causa):
+def busca_causa(causa, tribunal_en_lista, fur_en_lista):
     partes = causa.split('-')
     # Ingresa la causa a buscar
     Select(driver.find_element(By.ID, 'tipoMisCauFam')).select_by_value(partes[0])
@@ -61,66 +62,76 @@ def busca_causa(causa):
     # Espera el resultado
     time.sleep(1)
 
-    # Extrae Rit y Tribunal de la tabla
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    col = soup.find('table', id='dtaTableDetalleMisCauFam').find('tbody').find('tr').find_all('td')
-    if len(col) == 7:
-        rit = col[1].get_text().strip()
-        tribunal = nom_tribunal(col[2].get_text().strip())
+    furPJUD = ''
+    tramite = ''
+    desc = ''
+
+    rows = driver.find_elements(By.XPATH, "//table[@id='dtaTableDetalleMisCauFam']/tbody/tr")
+    
+    for row in rows:
+        tds = row.find_elements(By.TAG_NAME, 'td')
+        if len(tds) == 7:
+            tribunal_en_tabla = nom_tribunal(tds[2].text.strip())
+            if tribunal_en_tabla == tribunal_en_lista:
+                tds[0].find_element(By.TAG_NAME, 'a').click()
+                WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, 'movimientoFam')))
+                cols = driver.find_elements(By.XPATH, "//div[@id='movimientoFam']/div/div/table/tbody/tr/td")
+                if len(cols) >= 7:
+                    tramite = cols[4].text.strip()
+                    desc = cols[5].text.strip()                    
+                    fur = cols[6].text.strip()
+                    fur = fur[:6] + fur[8:]
+                    furPJUD = fur.replace('/','-')
+                driver.find_element(By.CLASS_NAME, 'close').click()                  
+    
+    rit.append(causa)
+    tribunal.append(tribunal_en_lista)
+    ultima.append(fur_en_lista.strftime('%d-%m-%y'))
+    ultimaPJUD.append(furPJUD)
+    if ultima[-1] != furPJUD:
+        revisar.append('Revisar ' + causa + ': ' + tramite + ', ' + desc)
     else:
-        rit = 'No encontrado'
-        tribunal = 'No encontrado'
+        revisar.append('-')
 
-    # Abre el modal con información sobre la causa
-    driver.find_element(By.XPATH, "//table[@id='dtaTableDetalleMisCauFam']/tbody/tr/td/a").click()
-    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, 'movimientoFam')))
- 
-    # Extrae fecha de última resolución
-    soupModal = BeautifulSoup(driver.page_source, 'html.parser')
-    filaModal = soupModal.find('div', id='movimientoFam').find('table').find('tbody').find('tr')
-    if filaModal == None:
-        ultima = ''
-    else:
-        celdas = filaModal.find_all('td')
-        fur = celdas[6].get_text().strip()
-        fur = fur[:6] + fur[8:]
-        ultima = fur.replace('/','-')
-    driver.find_element(By.CLASS_NAME, 'close').click()
-    return (rit, tribunal, ultima)
+def revisa_abogado(usuario, pwd, lista):
+    login(usuario, pwd)
+    for n in lista.index:
+        busca_causa(lista['ROL/RIT'][n], lista['TRIBUNAL'][n], lista['FUR'][n])
+    logout()
+    guarda_excel()
 
+def revisa_usuarios(lista):
+    for n in lista.index:
+        login(lista['RUT_PATROCINADO'][n], lista['CLAVE'][n])
+        busca_causa(lista['ROL/RIT'][n], lista['TRIBUNAL'][n], lista['FUR'][n])
+        logout()
+    guarda_excel()
 
-df = pd.read_excel('causas.xls','Asignadas')
+def guarda_excel():
+    dict =  {'Rit': rit, 'Tribunal': tribunal, 'FUR': ultima, 'FUR PJUD': ultimaPJUD, 'Revisar': revisar}
+    df = pd.DataFrame.from_dict(dict)
+    df.to_excel('revisar.xlsx')
+
+df = pd.read_excel('Carolina causas asignadas.xls','Asignadas')
 activas = df[df['T/V'].isin(['D', 'T/R', 'V'])]
 
 caro = activas[activas.ACCESO == 'CC']
 franco = activas[activas.ACCESO == 'FG']
+usuarios = activas[activas.ACCESO == 'U']
 
 rit = []
 tribunal = []
-tribunalPJUD = []
 ultima = []
 ultimaPJUD = []
 revisar = []
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-login('13824572', 'Encanto1981')
 
-for n in caro.index:
-    r, t, u = busca_causa(caro['ROL/RIT'][n])
-    rit.append(r)
-    tribunal.append(caro['TRIBUNAL'][n])
-    tribunalPJUD.append(t)
-    ultima.append(caro['última resolución'][n].strftime('%d-%m-%y'))
-    ultimaPJUD.append(u)
-    if (tribunal[-1] != t) or (ultima[-1] != u):
-        revisar.append('Revisar ' + r)
-    else:
-        revisar.append('')
+revisa_usuarios(usuarios)
+revisa_abogado('138245721', 'POllito-1010', caro)
+revisa_abogado('128622497', 'Catalinabarra2614.', franco)
 
-
-dict =  {'Rit': rit, 'Tribunal': tribunal, 'Tribunal PJUD': tribunalPJUD, 'FUR': ultima, 'FUR PJUD': ultimaPJUD, 'Revisar': revisar}
-df = pd.DataFrame.from_dict(dict)
-
-df.to_excel('resultado.xlsx')
 driver.close()
+
+
