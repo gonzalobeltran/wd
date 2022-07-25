cd60b1af700078ddb1841bee7845205dbee73ba3
Gonzalo Beltrán <gonzalobeltran@gmail.com>
2022-07-25 00:47:03 -0400

    Modifica excel directamente

--- pjud.py
+++ pjud.py
@@ -1,21 +1,43 @@
 
 import time, datetime
+from attr import attr, attrib
 import pandas as pd
+from openpyxl import load_workbook
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
+class Causa:
+    def __init__(self, fila, rit, tribunal, acceso, rut, clave, fur):
+        self.fila = fila
+        self.rit = rit
+        self.tribunal = tribunal
+        self.acceso = acceso
+        self.rut = rut
+        self.clave = clave
+        self.fur = fur
+
 def login(usuario, pwd):
     driver.get("https://oficinajudicialvirtual.pjud.cl/home/index.php")
     
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'dropbtn')))
     driver.find_element(By.CLASS_NAME, "dropbtn").click()
     driver.find_element(By.XPATH, "//div[@id='myDropdown']/a").click()
     driver.find_element(By.ID, "uname").send_keys(usuario)
     driver.find_element(By.ID, "pword").send_keys(pwd)
     driver.find_element(By.ID, "login-submit").click()
     time.sleep(4)
+
+    # Verifica si la clave fue incorrecta
+    try:
+        driver.find_element(By.CLASS_NAME, 'gob-response-error')
+    except:
+        pass
+    else:
+        print('Error')
+        return(False)
 
     # Cierra el modal de bienvenida si aparece
     try:
@@ -27,22 +49,26 @@ def login(usuario, pwd):
     driver.find_element(By.CLASS_NAME, "fa-university").click()
     time.sleep(2)
 
+    # Cierra el modal informativo si aparece
     try:
         driver.find_element(By.ID, 'btnEntendidoMisCausas').click()
         time.sleep(1)
     except:
         pass
 
-    driver.find_element(By.ID, "familiaTab").click()
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, 'familiaTab')))
+    driver.find_element(By.ID, 'familiaTab').click()
     time.sleep(1)
 
     driver.find_elements(By.CLASS_NAME, "switch")[6].click()
-    time.sleep(2)
+    time.sleep(3)
 
     # Selecciona todos los tipos de causas
     estadoCausas = Select(driver.find_element(By.ID, 'estadoCausaMisCauFam'))
     for i in range(12):
         estadoCausas.select_by_index(str(i))
+    
+    return(True)
 
 def logout():
     driver.find_element(By.XPATH, "//a[@onclick='salir();']").click()
@@ -62,8 +88,8 @@ def nom_tribunal(t):
         res = 'CENTRO DE MEDIDAS CAUTELARES DE SANTIAGO'
     return(res)
 
-def busca_causa(causa, tribunal_en_lista, fur_en_lista):
-    partes = causa.split('-')
+def busca_causa(causa):
+    partes = causa.rit.split('-')
     # Ingresa la causa a buscar
     Select(driver.find_element(By.ID, 'tipoMisCauFam')).select_by_value(partes[0])
     driver.find_element(By.ID, 'rolMisCauFam').clear()
@@ -76,16 +102,18 @@ def busca_causa(causa, tribunal_en_lista, fur_en_lista):
     time.sleep(2)
 
     furPJUD = ''
+    revisar = ''
     tramite = ''
     desc = ''
 
+    WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, 'dtaTableDetalleMisCauFam')))
     rows = driver.find_elements(By.XPATH, "//table[@id='dtaTableDetalleMisCauFam']/tbody/tr")
     
     for row in rows:
         tds = row.find_elements(By.TAG_NAME, 'td')
         if len(tds) == 7:
             tribunal_en_tabla = nom_tribunal(tds[2].text.strip())
-            if tribunal_en_tabla == tribunal_en_lista:
+            if tribunal_en_tabla == causa.tribunal:
                 tds[0].find_element(By.TAG_NAME, 'a').click()
                 WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, 'movimientoFam')))
                 cols = driver.find_elements(By.XPATH, "//div[@id='movimientoFam']/div/div/table/tbody/tr/td")
@@ -93,66 +121,66 @@ def busca_causa(causa, tribunal_en_lista, fur_en_lista):
                     tramite = cols[4].text.strip()
                     desc = cols[5].text.strip()                    
                     fur = cols[6].text.strip()
-                    fur = fur[:6] + fur[8:]
-                    furPJUD = fur.replace('/','-')
-                driver.find_element(By.CLASS_NAME, 'close').click()                  
+                    furPJUD = datetime.datetime.strptime(fur, '%d/%m/%Y')
+                driver.find_element(By.CLASS_NAME, 'close').click()  
+                time.sleep(1)                
     
-    rit.append(causa)
-    tribunal.append(tribunal_en_lista)
-    ultima.append(fur_en_lista.strftime('%d-%m-%y'))
-    ultimaPJUD.append(furPJUD)
-    if ultima[-1] != furPJUD:
-        revisar.append('Revisar ' + causa + ': ' + tramite + ', ' + desc)
-    else:
-        revisar.append('')
+    if causa.fur != furPJUD:
+        revisar = tramite + ', ' + desc
+    hoja.cell(causa.fila, 15, furPJUD)
+    hoja.cell(causa.fila, 16, revisar)
+    guarda_excel()
 
-def revisa_abogado(usuario, pwd, lista, archivo):
-    rit = []
-    tribunal = []
-    ultima = []
-    ultimaPJUD = []
-    revisar = []
+def revisa_abogado(usuario, pwd, lista):
+    if len(lista) == 0:
+        return
     login(usuario, pwd)
-    for n in lista.index:
-        busca_causa(lista['ROL/RIT'][n], lista['TRIBUNAL'][n], lista['FUR'][n])
+    for causa in lista:
+        busca_causa(causa)
     logout()
-    guarda_excel(archivo)
 
 def revisa_usuarios(lista):
-    rit = []
-    tribunal = []
-    ultima = []
-    ultimaPJUD = []
-    revisar = []
-    for n in lista.index:
-        login(lista['RUT_PATROCINADO'][n], lista['CLAVE'][n])
-        busca_causa(lista['ROL/RIT'][n], lista['TRIBUNAL'][n], lista['FUR'][n])
-        logout()
-    guarda_excel('Usuarios.xlsx')
+    if len(lista) == 0:
+        return
+    for causa in lista:
+        res = login(causa.rut, causa.clave)
+        if (res):
+            busca_causa(causa)
+            logout()
+        else:
+            hoja.cell(causa.fila, 16, 'Clave incorrecta')
+            guarda_excel()
 
-def guarda_excel(nombre):
-    dict =  {'Rit': rit, 'Tribunal': tribunal, 'FUR': ultima, 'FUR PJUD': ultimaPJUD, 'Revisar': revisar}
-    df = pd.DataFrame.from_dict(dict)
-    df.to_excel(nombre)
+def guarda_excel():
+    wb.save('Carolina causas.xlsx')
 
-df = pd.read_excel('Carolina causas.xls','Asignadas')
-activas = df[df['T/V'].isin(['D', 'T/R', 'V'])]
+df = pd.read_excel('Carolina causas.xlsx','Asignadas')
+por_revisar = df[df['FUR PJUD'].isnull() & df['REVISAR'].isnull()]
+activas = por_revisar[por_revisar['T/V'].isin(['D', 'T/R', 'V'])]
 
-franco = activas[activas.ACCESO == 'FG']
-caro = activas[activas.ACCESO == 'CC']
-usuarios = activas[activas.ACCESO == 'U']
+print(activas)
 
-rit = []
-tribunal = []
-ultima = []
-ultimaPJUD = []
-revisar = []
+caro = []
+franco = []
+usuarios = []
+
+for i in activas.index:
+    causa = Causa(i+2, activas['ROL/RIT'][i], activas['TRIBUNAL'][i], activas['ACCESO'][i], activas['RUT_PATROCINADO'][i], activas['CLAVE'][i], activas['FUR'][i])
+    if causa.acceso == 'CC':
+        caro.append(causa)
+    if causa.acceso == 'FG':
+        franco.append(causa)       
+    if causa.acceso == 'U':
+        usuarios.append(causa)
+
+wb = load_workbook(filename = 'Carolina causas.xlsx')
+hoja = wb['Asignadas']
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-# revisa_abogado('138245721', 'POllito-1010', caro, 'Caro.xlsx')
-# revisa_abogado('128622497', 'Catalinabarra2614.', franco, 'Franco.xlsx')
+revisa_abogado('138245721', 'POllito-1010', caro)
+revisa_abogado('128622497', 'Catalinabarra2614.', franco)
 revisa_usuarios(usuarios)
 
 driver.close()
