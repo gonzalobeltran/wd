cb8eac6c72ff3c64676720544c83557207d29385
Gonzalo Beltrán <gonzalobeltran@gmail.com>
2022-07-30 14:21:34 -0400

    Actualiza liquidaciones

--- liquidaciones.py
+++ liquidaciones.py
@@ -1,47 +1,31 @@
 
-import time, re
-import pandas as pd
-from bs4 import BeautifulSoup
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://sso.uc.cl")
-elem = driver.find_element(By.ID, "username")
-elem.clear()
-elem.send_keys("gbeltran")
-
-elem = driver.find_element(By.ID, "password")
-elem.clear()
-elem.send_keys("1787ZSIomYyi")
-
-elem.send_keys(Keys.RETURN)
+driver.find_element(By.ID, "username").send_keys("gbeltran")
+driver.find_element(By.ID, "password").send_keys("1787ZSIomYyi")
+driver.find_element(By.CLASS_NAME, "btn-submit").click()
 time.sleep(1)
 
 driver.get("https://sso.uc.cl/cas/login?service=https://rrhh.uc.cl/psp/hcm91prd/?cmd=start%26languageCd=ESP")
-elem = driver.find_element(By.ID, "CUS_AUTOSERVICIO_EMPLEADO$0")
-elem.click()
-time.sleep(1)
+driver.find_element(By.ID, "CUS_AUTOSERVICIO_EMPLEADO$0").click()
+time.sleep(2)
 
-elem = driver.find_element(By.ID, "CUS_REMUNERACIONES$1")
-elem.click()
+driver.find_element(By.ID, "CUS_REMUNERACIONES$1").click()
 
-pagina = driver.page_source
-
-soup = BeautifulSoup(pagina, 'html.parser')
-
+dates = list(map(lambda x: x.text, driver.find_elements(By.XPATH, "//a[contains(@id,'VIEW')]")[2:]))
 fechas = []
-for fecha in soup.findAll('a', id=re.compile('VIEW.*')):
-    if (fecha.get_text() != 'Fecha Pago'):
-        fechas.append(fecha.get_text())
+for f in dates:
+    partes = f.split('/')
+    fechas.append(partes[1] + '-' + partes[0] + '-' + partes[2])
 
-neto = []
-for pago in soup.findAll('span', id=re.compile('CUS_LIQSUEL_WRK_CUS_LCL_PAGO_NETO.*')):
-    neto.append(pago.get_text())
+montos = list(map(lambda x: x.text, driver.find_elements(By.XPATH, "//span[contains(@id,'CUS_LIQSUEL_WRK_CUS_LCL_PAGO_NETO')]")))
 
-dict = {'Fecha': fechas, 'Sueldo': neto}
-df = pd.DataFrame.from_dict(dict)
-df.to_excel('liquidaciones.xlsx')
+for i in range(0, len(fechas)):
+    print(fechas[i] + ': ' + montos[i])
 
 driver.close()
